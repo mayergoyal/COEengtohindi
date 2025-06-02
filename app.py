ba7bc1c5efb285f09cace868f74cf3be8dd7c2d3
MAYER GOYAL <goyalmayer2@gmail.com>
2025-06-03 02:35:36 +0530

    merge bhi ho gya.. transcription + translation

---
+++ app.py
@@ -0,0 +1,39 @@
+from transformers import MarianTokenizer as token, MarianMTModel as mtmodel
+import re
+import chardet
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # 0 = all logs, 1 = warnings, 2 = errors, 3 = fatal
+
+def clean_pehle(text:str)->str:
+    text=text.strip()
+    text = re.sub(r"\[\d{2}:\d{2}:\d{2}\.\d{3}\s*-->\s*\d{2}:\d{2}:\d{2}\.\d{3}\]\s*", "", text)
+    if(text and not text.endswith(('.','!','?'))):
+        text+='.'
+    print(" cleaned text is ",text)
+    return text
+def translate_to_hindi(text:str)-> str:
+    
+    tokenizer = token.from_pretrained("./model")
+    model = mtmodel.from_pretrained("./model")
+    
+    #time to tokenize the input text
+    inputs=tokenizer(text,return_tensors='pt',padding=True)
+    
+    # now ab transltae karte hain
+    translated=model.generate(**inputs)
+    #decoding the translation
+    hindi_text=tokenizer.decode(translated[0],skip_special_tokens=True)
+    return hindi_text
+
+if __name__=="__main__":
+    with open(".\\whisper.cpp\\output.txt", "rb") as file:
+        inp = file.read()
+        encoding=chardet.detect(inp)['encoding']
+    inp=inp.decode(encoding)
+    print("Translating to Hindi ...")
+    print("Input text is ", inp)
+    inp = clean_pehle(inp)
+    print(" cleaned Input text is ", inp)
+    output=translate_to_hindi(inp)
+    print("here u go ")
+    print(output)
---
+++ new.py
@@ -0,0 +1,10 @@
+from transformers import MarianTokenizer, MarianMTModel
+
+model_name = "Helsinki-NLP/opus-mt-en-hi"  # English → Hindi model
+
+# Download and save both tokenizer + model to ./model
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+
+tokenizer.save_pretrained("./model")
+model.save_pretrained("./model")
